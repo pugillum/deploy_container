bd449f075f5023d461ca1dea2cc074cf1577d846
Travis Dent <15220784+pugillum@users.noreply.github.com>
2021-12-23 16:12:09 +0100

    Add deployment

---
+++ infra/provider.tf
@@ -0,0 +1,16 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
---
+++ infra/variables.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "resource_group" {
+  type    = string
+  default = "travis-container-deploy"
+}
+
