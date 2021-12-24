dfdb99ae038fd5ef9d312cd24bad8bbdb5da0d0f
Travis Dent <15220784+pugillum@users.noreply.github.com>
2021-12-24 13:00:37 +0100

    Cleanup and shift to basic

--- infra/main.tf
+++ infra/main.tf
@@ -7,19 +7,10 @@ resource "random_id" "aci_unique_string" {
   byte_length = 2
 }
 
-# resource "azurerm_container_group" "deploy_container_group" {
-#   name                = "cg-deploy-container"
-#   location            = azurerm_resource_group.rg_deploy_container.location
-#   resource_group_name = azurerm_resource_group.rg_deploy_container.name
-#   ip_address_type     = "public"
-#   dns_name_label      = "aci-test-${random_id.aci_unique_string.hex}"
-#   os_type             = "Linux"
-# }
-
 resource "azurerm_container_registry" "acr" {
   name                = "crdeploycontainer"
   resource_group_name = azurerm_resource_group.rg_deploy_container.name
   location            = azurerm_resource_group.rg_deploy_container.location
-  sku                 = "Standard"
+  sku                 = "Basic"
   admin_enabled       = false
 }
